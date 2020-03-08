2e00492e5d870485359dcb57a8c87a94bafa3242
DandeOne <dande2@o2.pl>
2020-03-08 19:22:29 +0100

    Added new graph with statistics for all pokemons. Used layout to show one graph in each row and next to them temporarly I'm writing json data on hovered point

--- dash_analysis.py
+++ dash_analysis.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+from dash.dependencies import Input, Output
+import json
 
 pokemon_dataset = pd.read_csv('pokemon.csv')
 
@@ -21,22 +23,62 @@ def createdashboard(app):
 
 def createlayout(appdash):
     appdash.layout = html.Div([
-        dcc.Graph(
-            id='Legendary_Pokemons',
-            figure={
-                'data': [
-                    go.Scatter(x=pokemon_dataset.loc[pokemon_dataset['is_legendary'] == True, 'attack'],
-                               y=pokemon_dataset.loc[pokemon_dataset['is_legendary'] == True, 'defense'],
-                               mode='markers'
-                               )
-                ],
-                'layout': go.Layout(
-                    title='Legendary Pokemons statistics',
-                    xaxis={'title': 'Attack'},
-                    yaxis={'title': 'Defense'},
-                    hovermode='closest'
-                )
-            }
-        )
+
+        html.Div([
+            html.Div(
+
+                dcc.Graph(
+                    id='Legendary_Pokemons',
+                    figure={
+                        'data': [
+                            go.Scatter(x=pokemon_dataset.loc[pokemon_dataset['is_legendary'] == True, 'attack'],
+                                       y=pokemon_dataset.loc[pokemon_dataset['is_legendary'] == True, 'defense'],
+                                       mode='markers'
+                                       )
+                        ],
+                        'layout': go.Layout(
+                            title='Legendary Pokemons statistics',
+                            xaxis={'title': 'Attack'},
+                            yaxis={'title': 'Defense'},
+                            hovermode='closest'
+                        )
+                    }
+                 ),
+                style={'width':'70%', 'float':'left'}, className='eight columns'
+            ),
+
+            html.Div(html.Pre(id='hover-data', style={'paddingTop': 35}), className='four columns')
+        ],
+        className='row'
+        ),
+
+
+        html.Div([
+            html.Div(
+                dcc.Graph(
+                    id='All_Pokemons',
+                    figure={
+                        'data': [
+                            go.Scatter(x=pokemon_dataset.loc[:, 'attack'],
+                                       y=pokemon_dataset.loc[:, 'defense'],
+                                       mode='markers'
+                                       )
+                        ],
+                        'layout': go.Layout(
+                            title='All Pokemons statistics',
+                            xaxis={'title': 'Attack'},
+                            yaxis={'title': 'Defense'},
+                            hovermode='closest'
+                        )
+                    }), style={'width':'70%', 'float':'left'},
+                        className='eleven columns'
+                ),
+            html.Div(html.Pre(id='hover-data2', style={'paddingTop': 35}), className='four columns')
+        ],
+        className='row'
+        ),
+
+
     ])
     return appdash
+
--- pokemonwebapp.py
+++ pokemonwebapp.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app_dash = dash_analysis.createdashboard(app)
 app_dash = dash_analysis.createlayout(app_dash)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,6 +16,20 @@ def index():
 def dashboard():
     return render_template('dash_template.html', dash_url=app_dash.routes_pathname_prefix)
 
+
+@app_dash.callback(dash_analysis.Output('hover-data', 'children'),
+                   [dash_analysis.Input('Legendary_Pokemons', 'hoverData')])
+def callback_image(hoverData):
+    return dash_analysis.json.dumps(hoverData, indent=2)
+
+
+@app_dash.callback(dash_analysis.Output('hover-data2', 'children'),
+                   [dash_analysis.Input('All_Pokemons', 'hoverData')])
+def callback_image2(hoverData):
+    return dash_analysis.json.dumps(hoverData, indent=2)
+
+
+app_dash.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
     app.run(debug=True)
